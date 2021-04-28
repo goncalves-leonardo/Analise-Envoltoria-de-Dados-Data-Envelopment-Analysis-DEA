0a059e0e4eb48e91b2b264a6e8621baca3fde0f5
goncalves-leonardo <30481716+goncalves-leonardo@users.noreply.github.com>
2021-04-28 14:42:29 -0300

    Update Script_Utilizacao_DEA_Pacote_ Benchmarking.R

--- Script_Utilizacao_DEA_Pacote_ Benchmarking.R
+++ Script_Utilizacao_DEA_Pacote_ Benchmarking.R
@@ -1,7 +1,26 @@
 rm(list=ls(all=TRUE)) #apagar todas listas e variáveis
 getwd() #importante saber qual o diretório de trabalho em utilização
-setwd('C:/Users/USUARIO/Documents/R/PASTA') #Definir uma pasta de trabalho
+Pasta.de.Trabalho <- 'C:/Users/leogp/Documents/R/Leonardo_Arquivos/DEA_2021'
+setwd(Pasta.de.Trabalho) #Definir uma pasta de trabalho
 dir() # ver quais os arquivos existentes no diretório em uso
+
+dados <- read.csv(choose.files(), header = TRUE, sep = ",") # abrir o arquivo ‘.csv’ que contém as variáveis
+head(dados)  
+glimpse(dados) #mostrar o tipo de cada variável do data frame
+
+#==================== Definir as variáveis INSUMOs x
+insumos <- as.matrix(with(dados, cbind(dados$Input1)))
+
+#==================== Definir as variáveis PRODUTOs y
+produtos <- as.matrix(with(dados, cbind(dados$Output1, 
+                                        dados$Output2,
+                                        dados$Output3)))
+
+#==================== Definir a variável que contém o nome das DMUs
+dmu <- dados$ï..DMU 
+
+# ATENÇÃO! Desta etapa para frente poderá selecionar tudo e clicar em RUN
+
 #==================== Instalar e carregar os pacotes necessários
 if(!require(dplyr)) install.packages("dplyr")
 library(dplyr) 
@@ -12,18 +31,15 @@ library(corrplot)
 if(!require(readxl)) install.packages("readxl")
 library(readxl)
 
-#==================== Leitura do arquivo ‘.csv’ que contém as variáveis
-dados <- read.csv(choose.files(), header = TRUE, sep = ",")
-head(dados)  
-glimpse(dados) # mostrar o tipo de cada variável do data frame
-# Criar matriz de correlação:
-matriz <-  cor(dados[2:5], method = "spearman") # Valores dentro dos colchetes determinam as colunas da matriz a serem usadas na análise
-# Resumidamente, Spearman ou kendall para não paramétrica e pearson para paramétrica
+
+# Início - criar matriz de correlação:
+matriz <-  cor(dados[2:5], method = "spearman") #entre colchetes determina as colunas a serem usadas na análise
+#spearman ou kendall para não paramétrica e pearson para paramétrica
 head(matriz)
 # Arredondando para duas casas decimais:
-matriz.correlacao.variaveis <- round(cor(dados[2:5], method = "spearman"), 2)
+matriz.correlacao.variaveis <- round(cor(dados[2:5], method = "spearman"), 2)#spearman ou pearson
 head(matriz.correlacao.variaveis)
-## Criando a representaçãoo gráfica de correlação das variáveis
+# Criando a representaçãoo gráfica da correlação das variáveis
 par(mfrow=c(1,1))
 corrplot(matriz.correlacao.variaveis, method="pie", 
          type="upper", order="alphabet",add = FALSE, 
@@ -37,6 +53,7 @@ matriz.correlacao.variaveis <- as.data.frame(matriz.correlacao.variaveis)
 #==================== Outras representações por meio de matriz e gráficos
 if(!require(Hmisc)) install.packages("Hmisc")
 library(Hmisc) 
+#A função rcorr no pacote Hmisc produz correlações/covariáveis e níveis de significância para correlações de pearson e spearman. No entanto, a entrada deve ser uma matriz e a exclusão em pares é usada.
 matriz.Hmisc <- dados[2:5]
 head(matriz.Hmisc)
 matriz.Hmisc<- rcorr(as.matrix(matriz.Hmisc), type ="spearman")
@@ -46,7 +63,7 @@ matriz.Hmisc$r # matriz de coeficiente de correlação
 matriz.Hmisc$P # matriz de valor p
 matriz.Hmisc$n # matriz número de variáveis
 
-#==================== plot
+#==================== plot - marcar x para as que não tiverem relação
 corrplot(matriz.Hmisc$r,p.mat=matriz.Hmisc$P,sig.level=0.05,method="pie",type="upper",diag=FALSE)
 
 #==================== alternativa para matriz
@@ -56,23 +73,31 @@ library(PerformanceAnalytics)
 md<- dados[,c(2:5)] # filtrando as colunas
 md
 
-chart.Correlation(md, histogram = TRUE,method ="spearman")
+chart.Correlation(md, histogram = TRUE, method ="spearman")
 
-help(package="Benchmarking") #Para ler sobre os pacotes a serem utilizados
-?dea 
-?Benchmarking 
+#plot padrão
+if(!require(psych)) install.packages("psych")
+library(psych)
+describe.by(md, group=NULL,mat=FALSE,type=3,digits=15  )
+pairs.panels(md,
+             smooth = TRUE,      # If TRUE, draws loess smooths
+             scale = FALSE,      # If TRUE, scales the correlation text font
+             density = TRUE,     # If TRUE, adds density plots and histograms
+             ellipses = TRUE,    # If TRUE, draws ellipses
+             method = "spearman", # Correlation method (also "spearman" or "kendall")
+             pch = 21,           # pch symbol
+             lm = FALSE,         # If TRUE, plots linear fit rather than the LOESS (smoothed) fit
+             cor = TRUE,         # If TRUE, reports correlations
+             jiggle = FALSE,     # If TRUE, data points are jittered
+             factor = 2,         # Jittering factor
+             hist.col = 4,       # Histograms color
+             stars = TRUE,       # If TRUE, adds significance level with stars
+             ci = TRUE)          # If TRUE, adds confidence intervals
 
-#==================== Definir as variáveis INSUMOs x
-insumos <- as.matrix(with(dados, cbind(dados$input1)))
 
-#==================== Definir as variáveis PRODUTOs y
-produtos <- as.matrix(with(dados, cbind(dados$Output1, 
-                                        dados$Output2,
-                                        dados$Output3)))
-
-#==================== Definir a variável que contém o nome das DMUs
-dmu <- dados$NomeDMU 
-
+#help(package="Benchmarking") # Início da aplicação DEA - Para ler sobre os pacotes a serem utilizados retire o '?' e clique em RUN
+#?dea 
+#?Benchmarking 
 #==================== Modelo BCC Orientado ao Input
 bcc.in <- dea(insumos, produtos, RTS="vrs", ORIENTATION="in", SLACK=TRUE) # eff BCC insumo orientado com folgas
 summary(bcc.in)
@@ -83,8 +108,10 @@ which(bcc.in$eff == 1 & !bcc.in$slack)
 dmus.pares.insumo <-  data.frame(DMU=dmu, round(lambda(bcc.in),2))
 head(dmus.pares.insumo,10)
 dt.eff.bcc.in.insumo<- data.frame("DMU"=dmu,"eff"=round(bcc.in$eff,3), "ins"=insumos, "rad"=bcc.in$eff * insumos, "fol"=bcc.in$sx, "alv"= insumos - bcc.in$sx)
+head(dt.eff.bcc.in.insumo)
 
 dt.eff.bcc.in.produto<- data.frame("DMU"=dmu,"eff"=round(bcc.in$eff,3), "ins"=produtos, "rad"=bcc.in$eff * produtos,"fol"=bcc.in$sy, "alv"=produtos + bcc.in$sy)
+head(dt.eff.bcc.in.produto)
 
 #==================== Modelo BCC Orientado ao Output
 bcc.out <- dea(insumos, produtos, RTS="vrs", ORIENTATION="out", SLACK=TRUE)
@@ -101,11 +128,10 @@ DMU.eff
 somente.dmus.eff.bcc.out <- data.frame(DMU=dmu[DMU.eff],LINHA=DMU.eff)#mostrar nome das DMU's eficientes
 head(somente.dmus.eff.bcc.out)
 
-dt.eff.bcc.out.insumo <- data.frame("DMU"=dmu, "eff"=round(1/bcc.out$eff,3), "ins"=insumos, "rad"=round((1/bcc.out$eff)*insumos,2), "Poupar"=round(insumos-(1/bcc.out$eff*insumos),2), "fol"=round(bcc.out$sx*-1,2), "alv"=round(insumos - bcc.out$sx,2))
+dt.eff.bcc.out.insumo <- data.frame("DMU"=dmu, "eff"=round(1/bcc.out$eff,3), "ins"=insumos, "rad"=round((1/bcc.out$eff)*insumos,2), "fol"=round(bcc.out$sx,2), "alv"=round(insumos - bcc.out$sx,2))
 head(dt.eff.bcc.out.insumo)
 
 dt.eff.bcc.out.produto <- data.frame("DMU"=dmu,"eff"=round(1/bcc.out$eff,3), "pro"=produtos, "rad"=round((bcc.out$eff * produtos)-produtos,2), "fol"=round(bcc.out$sy,2), "alv"=round((bcc.out$eff * produtos)+bcc.out$sy,2))
-
 head(dt.eff.bcc.out.produto)
 
 #====================  RENDIMENTOS DE ESCALA
@@ -125,10 +151,7 @@ rendimentos <- data.frame("DMU"=dmu,ee, "E_ESC"=round(ee$CRS/ee$VRS,3),"REND"=if
 
 head(rendimentos)
 
-#==================== Visualizar ordenado do maior para o menor pelo modelo VRS
-head(rendimentos[order(rendimentos$VRS, decreasing=TRUE),])
-
-#==================== Gráfico 1
+#==================== Dados para o Gráfico 1
 #Selecionar as colunas que quer representar no gráfico
 dados.grafico.rendimento1 <-rendimentos[,c("CRS", "VRS","E_ESC")]
 #renomear coluna
@@ -138,18 +161,17 @@ glimpse(dados.grafico.rendimento1)
 
 head(dados.grafico.rendimento1)
 
+#==================== Plotando o Gráfico 1
 barplot(t(dados.grafico.rendimento1), names.arg = dmu, cex.axis=1,cex.names=0.9, plot = TRUE, beside = TRUE, axis.lty = 1, offset = 0, col = c("#345082","#608AD4", "#C2D0EA"), horiz = FALSE,axisnames = TRUE, xlab = NULL, ylab = NULL)
-
-title(main = "Rendimentos de Escala, CRS e VRS no modelo orientado ao output", font = 1)
-text(5,.98, "Fronteira de eficiência", col = "black")
-abline(h = 1, col = "gray60",lty="dotted")#dashed ou dotted
-
-legend(x=110, y=0.95, xpd=TRUE, ncol=1, legend=c("RS", "VRS", "Escala"), fill=c("#345082","#608AD4", "#C2D0EA"), bty="n",cex=0.5)
-
-#nota de rodapé no gráfico
+title(main = "Rendimentos com retornos constantes, variáveis e de escala: orientação ao output", font = 1)
+text(30,.98, "Fronteira de eficiência", col = "black")
+abline(h = 1, col = "gray60",lty="dotted") #dashed ou dotted
+legend(x=90, y=.2, xpd=TRUE, ncol=1, legend=c("CRS", "VRS", "Escala"), fill=c("#345082","#608AD4", "#C2D0EA"), bty="t",cex=0.5)
+#==================== Nota de rodapé no gráfico 1
 mtext(side=4, at=par("usr")[3], adj=0, cex=0.8, col="gray40", line=0, text=paste("Pereira, Leonardo --", format(Sys.time(), "%d/%m/%Y %H:%M:%S --"), R.version.string))
 
-#==================== Dados para o gráfico 2
+
+#==================== Dados para o Gráfico 2
 #Selecionar as colunas que quer representar no gráfico
 dados.grafico.rendimento2 <-rendimentos[,c("VRS", "CRS","E_ESC")]
 #renomear coluna
@@ -157,19 +179,17 @@ dados.grafico.rendimento2 <- rename(dados.grafico.rendimento2,Escala=E_ESC)
 print(dados.grafico.rendimento2)
 glimpse(dados.grafico.rendimento2)
 
-barplot(t(dados.grafico.rendimento2), names.arg = dmu, cex.axis=1,cex.names=0.9, plot = TRUE, beside = FALSE, axis.lty = 1, offset = 0, col = c("#345082","#608AD4", "#C2D0EA"), horiz = FALSE,axisnames = TRUE, xlab = NULL, ylab = NULL)
-
-title(main = "Rendimentos de Escala, CRS e VRS no modelo orientado ao output", font = 1)
-text(-0.5,.95, "VRS", col = "black")
+#==================== Plotando o gráfico 2
+barplot(t(dados.grafico.rendimento2), names.arg = dmu, cex.axis=1,cex.names=0.7, plot = TRUE, beside = FALSE, axis.lty = 1, offset = 0, col = c("#345082","#608AD4", "#C2D0EA"), horiz = FALSE,axisnames = TRUE, xlab = NULL, ylab = NULL)
+title(main = "Rendimentos com retornos constantes, variáveis e de escala: orientação ao output", font = 1)
+text(-0.5,.95, "VRS", col = "black", cex = .7)
 abline(h = 1, col = "black",lty="dashed")#dashed ou dotted
-text(-0.5,1.95, "CRS", col = "black")
+text(-0.5,1.95, "CRS", col = "black", cex = .7)
 abline(h = 2, col = "black",lty="dashed") 
-text(-0.1,2.95, "ESCALA", col = "black")
+text(-0.1,2.95, "ESCALA", col = "black", cex = .7)
 abline(h = 3, col = "black",lty="dashed")
-
 legend(x=28, y=2.9, xpd=TRUE, ncol=1, legend=c("VRS", "CRS", "Escala"), fill=c("#345082","#608AD4", "#C2D0EA"), bty="t",cex=.9)
-
-#==================== Nota de rodapé no gráfico
+#==================== Nota de rodapé no gráfico 2
 mtext(side=4, at=par("usr")[3], adj=0, cex=0.8, col="gray40", line=0, text=paste("Pereira, Leonardo -", format(Sys.time(), "%d/%m/%Y %H:%M:%S --"), R.version.string))
 
 #==================== Representação Gráfica da isoquanta
@@ -179,6 +199,74 @@ y <- produtos
 dea.plot(x,y, RTS="vrs", ORIENTATION="in-out",txt=dmu, main="Fronteira DEA BCC-O", lty="dotted",lwd=1,col="BLACK", fex=0.7,GRID=FALSE)
 dea.plot(x,y,RTS="crs",add=TRUE,lty="dotted",col="red")
 
-#==================== Exportar para Excel: cada sheet (guia) será como na sequência
+#==================== Pacote qcc - Gráfico de Pareto
+if(!require(qcc)) install.packages("qcc")
+library(qcc) 
 
-writexl::write_xlsx(list(dados, dt.eff.bcc.out.insumo, dt.eff.bcc.out.produto, rendimentos, dmus.pares.produto, somente.dmus.eff.bcc.out ), path = paste("Resultados_DEA_", format(Sys.time(),"%d_%m_%Y"),".xlsx", sep = "", collapse = NULL), col_names = TRUE, format_headers = TRUE, use_zip64 = FALSE)
+pareto.chart(dados.frequencia, ylab = "Freq. Ineficiência Relativa", xlab = "DMUs", ylab2="Porcentagem Acumulada",las=2)
+
+# Alterando a função do pacote qcc
+func.grafico.pareto <- function (data, plot = TRUE, ...) 
+{
+  call <- match.call(expand.dots = TRUE)
+  varname <- deparse(substitute(data))
+  data <- as.table(data)
+  if (length(dim(data)) > 1) 
+    stop("only one-dimensional object (table, vector, etc.) may be provided")
+  data <- sort(data, decreasing = TRUE, na.last = TRUE)
+  csum <- cumsum(data)
+  tab <- round(cbind(data, csum, data/max(csum, na.rm = TRUE) * 
+                       100, csum/max(csum, na.rm = TRUE) * 100),3)
+  colnames(tab) <- c("Freq.Absoluta", "Freq.Acumulada", "Perc.Individual", 
+                     "Perc.Acumulado")
+  names(dimnames(tab)) <- c("", paste("\nPareto chart analysis for", 
+                                      varname))
+  tab <- as.table(tab)
+  class(tab) <- c("pareto.chart", class(tab))
+  attr(tab, "call") <- call
+  attr(tab, "varname") <- varname
+  if (plot) 
+    plot(tab, ...)
+  return(tab)
+}
+
+# Utilizando a função com alterações
+dados.frequencia <- (rendimentos$VRS) #definir os dados para analisar a distribuição de frequência
+names(dados.frequencia) <- rendimentos$DMU
+frequencia.pareto.VRS <- data.frame(cbind( func.grafico.pareto(dados.frequencia, ylab = "Freq. DMUs Ineficientes")))
+
+# Analisar sob a ótica da Regra 80/20
+frequencia.pareto.VRS
+
+
+#==================== Exportar para Excel - primeira opção
+writexl::write_xlsx(list(dados, dt.eff.bcc.out.insumo, dt.eff.bcc.out.produto, rendimentos,
+                         dmus.pares.produto, somente.dmus.eff.bcc.out ), 
+                    path = paste("Resultados_DEA_", 
+                                 format(Sys.time(),"%d_%m_%Y"),".xlsx", 
+                                 sep = "", collapse = NULL), col_names = TRUE, 
+                    format_headers = TRUE, use_zip64 = FALSE)
+
+
+#==================== Função para Exportar para Excel: cada guia terá o nome das variáveis
+xlsx.SalvarNomeGuia <- function (file, ...)
+{
+  require(xlsx, quietly = TRUE)
+  objects <- list(...)
+  fargs <- as.list(match.call(expand.dots = TRUE))
+  objnames <- as.character(fargs)[-c(1, 2)]
+  nobjects <- length(objects)
+  for (i in 1:nobjects) {
+    if (i == 1)
+      write.xlsx(objects[[i]], file, sheetName = objnames[i],col.names = TRUE, 
+                 row.names = TRUE)
+    else write.xlsx(objects[[i]], file, sheetName = objnames[i],
+                    append = TRUE, col.names = TRUE, row.names = TRUE)
+  }
+}
+
+#==================== Exportar para Excel - segunda opção
+xlsx.SalvarNomeGuia("Resultados_DEA.xlsx",
+                    dados, dt.eff.bcc.out.insumo, dt.eff.bcc.out.produto, rendimentos,
+                    dmus.pares.produto, somente.dmus.eff.bcc.out, frequencia.pareto.VRS)
+
